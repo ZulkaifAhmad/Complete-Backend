b37329a9c1a059e0b9105ea63fd5731013310df3
ZulkaifAhmad <zulkaifkhan183@gmail.com>
2026-06-06 02:50:00 +0500

    Notes App Backend

---
+++ src/app.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const dbConnection = require("./db/db");
+const Notes = require("./db/schema");
+const app = express();
+
+dbConnection();
+
+let notes = [];
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  try {
+    const payload = req.body;
+    // notes.push(payload);
+
+    const save = await Notes.create(payload);
+
+    res.status(200).json({
+      message: "data saved in notes",
+      data: save,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(300).json({
+      Message: error,
+    });
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    let notes = await Notes.find();
+    console.log(notes);
+    res.status(200).json({
+      data: notes,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(300).json({
+      message: error,
+    });
+  }
+});
+
+app.patch("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const payload = req.body;
+
+    const note = await Notes.findOne({ _id: id });
+
+    if (!note) {
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    }
+
+    // update only if value exists
+    if (payload.title !== undefined) {
+      note.title = payload.title;
+    }
+
+    if (payload.content !== undefined) {
+      note.content = payload.content;
+    }
+
+    await note.save();
+
+    res.status(200).json({
+      message: "Data updated successfully",
+      data: note,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating data",
+      error: error.message,
+    });
+  }
+});
+
+app.delete("/:id", async(req, res) => {
+    try {
+        let { id } = req.params;
+        let note = await Notes.deleteOne({_id : id})
+        res.status(204).json({
+          message: "Note deleted successfully",
+          notes,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = app;
---
+++ src/db/schema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const noteSchema = mongoose.Schema({
+    title : {
+        type : String ,
+        required : true 
+    } , 
+    content : {
+        type : String  , 
+        required : true
+    }
+}, {timestamps : true})
+
+const Notes = mongoose.model("backend" , noteSchema);
+
+module.exports = Notes
+
